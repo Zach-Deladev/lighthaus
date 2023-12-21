98aefc430caa6978d68e095db2ce5c4e9180ff47
Zach Del <zach.delapenha.developer@gmail.com>
2023-12-21 02:51:48 +0000

    Added lighthaus music to spotify iframe

--- client/src/pages/Music.tsx
+++ client/src/pages/Music.tsx
@@ -7,7 +7,7 @@ const Music: React.FC = () => {
         <h1 className="text-5xl text-white font-semibold">Latest Songs</h1>
         <iframe
           className="my-10"
-          src="https://open.spotify.com/embed/artist/7EQ0qTo7fWT7DPxmxtSYEc?utm_source=generator&theme=0"
+          src="https://open.spotify.com/embed/artist/3mNZQiXEFcfuYutzuoO13W?utm_source=generator"
           width="100%"
           height="700"
           allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
